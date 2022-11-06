c34f5e2782371cb126a1f1e45acf69cdfb067273
Chris Marshall <chris@riftvalleysoftware.com>
2022-11-06 15:55:27 -0500

    Starting with a stub index.

---
+++ Sources/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>LGV_MeetingServer</title>
+    </head>
+    <body>
+        <h1>LGV_MeetingServer</h1>
+    </body>
+</html>
