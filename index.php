63c8166bd7d73c5974abf8d4a9bc08a26be5270b
Chris Marshall <chris@riftvalleysoftware.com>
2022-11-06 11:38:07 -0500

    Initial index page for testing and whatnot.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>LGV_MeetingServer Test Host</title>
+    </head>
+    <body>
+        <h1>LGV_MeetingServer Test Host</h1>
+    </body>
+</html>
