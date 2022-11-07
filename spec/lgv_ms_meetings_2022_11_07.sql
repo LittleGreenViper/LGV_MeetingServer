32d819281c291e57ad91f31bde7d9c271ce138c0
Chris Marshall <chris@riftvalleysoftware.com>
2022-11-07 07:26:59 -0500

    Updated the seed SQL

--- spec/lgv_ms_meetings_2022_11_07.sql
+++ spec/lgv_ms_meetings_2022_11_07.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 06, 2022 at 11:22 PM
+-- Generation Time: Nov 07, 2022 at 12:26 PM
 -- Server version: 5.7.34
 -- PHP Version: 8.0.8
 
@@ -31,19 +31,30 @@ DROP TABLE IF EXISTS `lgv_ms_meetings`;
 CREATE TABLE `lgv_ms_meetings` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `server_id` int(10) UNSIGNED NOT NULL,
-  `meeting_id` int(11) NOT NULL,
+  `meeting_id` int(10) UNSIGNED NOT NULL,
+  `organization_key` varchar(32) COLLATE utf8_bin DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `start_time` time DEFAULT NULL,
+  `weekday` tinyint(3) UNSIGNED DEFAULT NULL,
+  `single_occurrence_date` datetime DEFAULT NULL,
   `duration` int(10) UNSIGNED ZEROFILL DEFAULT NULL,
   `longitude` float DEFAULT NULL,
   `latitude` float DEFAULT NULL,
-  `weekday` tinyint(3) UNSIGNED DEFAULT NULL,
-  `organization_key` varchar(32) COLLATE utf8_bin DEFAULT NULL,
-  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tag0` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag1` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag2` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag3` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag4` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag5` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag6` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag7` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag8` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `tag9` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `comments` text COLLATE utf8_bin,
   `formats` text COLLATE utf8_bin,
   `physical_address` text COLLATE utf8_bin,
-  `virtual_information` text COLLATE utf8_bin
+  `virtual_information` text COLLATE utf8_bin,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -63,7 +74,18 @@ ALTER TABLE `lgv_ms_meetings`
   ADD KEY `longitude` (`longitude`),
   ADD KEY `latitude` (`latitude`),
   ADD KEY `weekday` (`weekday`),
-  ADD KEY `organization_key` (`organization_key`);
+  ADD KEY `organization_key` (`organization_key`),
+  ADD KEY `single_occurrence_date` (`single_occurrence_date`),
+  ADD KEY `tag0` (`tag0`),
+  ADD KEY `tag1` (`tag1`),
+  ADD KEY `tag2` (`tag2`),
+  ADD KEY `tag3` (`tag3`),
+  ADD KEY `tag4` (`tag4`),
+  ADD KEY `tag5` (`tag5`),
+  ADD KEY `tag6` (`tag6`),
+  ADD KEY `tag7` (`tag7`),
+  ADD KEY `tag8` (`tag8`),
+  ADD KEY `tag9` (`tag9`);
 
 --
 -- AUTO_INCREMENT for dumped tables
