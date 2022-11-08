19ea2f5dd0fe84ba38e4dd88e079ccc30eced340
Chris Marshall <chris@riftvalleysoftware.com>
2022-11-08 10:33:36 -0500

    Added a config file, for the "public" config values (like the TOMATO list).

---
+++ Sources/config/LGV_MeetingServer-Config.php
@@ -0,0 +1,30 @@
+<?php
+/***************************************************************************************************************************/
+/**
+    This is the main entrypoint file for the LGV_MeetingServer basic server-level unit tests.
+    
+    © Copyright 2022, <a href="https://littlegreenviper.com">Little Green Viper Software Development LLC</a>
+    
+    LICENSE:
+    
+    MIT License
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+    modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    The Great Rift Valley Software Company: https://riftvalleysoftware.com
+*/
+defined( 'LGV_MeetingServer_Files' ) or die ( 'Cannot Execute Directly' );	// Makes sure that this file is in the correct context.
+
+/// This is the "master list" of all the BMLT servers that TOMATO (the BMLT aggregator) uses. It is a JSON file.
+$_tomato_server_list_file_uri = "https://raw.githubusercontent.com/bmlt-enabled/tomato/master/rootServerList.json";
+?>
